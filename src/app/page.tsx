2465988b418840583b843207fc0dbecf6f737912
Dileep BC <dileep@aglinthq.com>
2024-12-21 00:48:51 +0530

    fix: portfolio setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,54 @@
-import Image from "next/image";
+// import { Icons } from '@/components/icons';
+// import OpenSource from '@/components/open-source';
+// import Project from '@/components/project';
+import Header from '@/components/header';
+import Project from '@/components/project';
+import { TimelineLayout } from '@/components/timeline/timeline-layout';
+import { Button } from '@/components/ui/button';
+import { CONFIG } from '@/config';
+// import { CONFIG } from '@/config';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className='animate-slide-from-down-and-fade-2 flex flex-col gap-12'>
+      <Header />
+      <div className=' space-y-2 px-4'>
+        <h2 className='font-semibold'>About me</h2>
+        <p className='leading-6 text-muted-foreground'>{CONFIG.description}</p>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+      <TimelineLayout />
+      <div className='flex animate-slide-from-down-and-fade-3 flex-col gap-3'>
+        <h2 className='px-4 font-semibold'>Projects</h2>
+        {CONFIG.projects
+          .filter((project) => project.featured)
+          .map((project, idx) => (
+            <Project
+              key={idx}
+              name={project.name}
+              icon={project.icon}
+              description={project.description}
+              image={project.image}
+              url={project.url}
+              tags={project.tags}
+              testimonial={project.testimonial}
+              github={project.github}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          ))}
+
+        <Button
+          asChild
+          className='ml-auto items-end text-muted-foreground underline hover:text-foreground'
+          variant={'link'}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <Link href='/projects'>
+            All projects
+            {/* <Icons.arrowUpRight className='inline-block size-4' /> */}
+          </Link>
+        </Button>
+      </div>
+      {/* <OpenSource /> */}
     </div>
   );
 }
