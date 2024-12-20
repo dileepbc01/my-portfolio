ab5ee310764dcae394e028f4194c31d436c24b54
dileepbc01 <166901803+dileepbc01@users.noreply.github.com>
2024-12-21 01:17:56 +0530

    workflow

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export', // <=== enables static exports
   reactStrictMode: true,
+  basePath: '/my-portfolio',
+
   /* config options here */
 };
 
