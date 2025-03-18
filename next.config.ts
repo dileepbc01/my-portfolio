6f1ad17792e3a81c476ea85f4162a9343d7aa853
Dileep B C <dileepbc01@gmail.com>
2025-03-18 19:04:07 +0530

    fix : broken packages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export', // <=== enables static exports
-  reactStrictMode: true,
-  basePath: '/my-portfolio',
-  distDir: 'out',
   /* config options here */
 };
 
